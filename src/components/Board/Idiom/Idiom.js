b2b82dfb3bfec6eb36fbc1b300896d09d387cc54
limboy <healdream@gmail.com>
2022-02-26 04:13:40 +0800

    add idiom component

--- src/components/Board/Idiom/Idiom.js
+++ src/components/Board/Idiom/Idiom.js
@@ -1,4 +1,26 @@
 import PropTypes from 'prop-types';
+
+const Letter = (props) => {
+  let bgColors = ['rgb(75 85 99)', 'rgb(202 138 4)', 'rgb(22 163 74)'];
+  let bgColor = props.checkResult
+    ? bgColors[props.checkResult]
+    : 'rgb(243 244 246)';
+  let textColor = props.checkResult ? 'rgb(243 244 246)' : 'rgb(75 85 99)';
+  return (
+    <div
+      className={`w-full h-full text-center align-middle text-2xl md:text-3xl`}
+      style={{ backgroundColor: bgColor, color: textColor }}
+    >
+      {props.letter.toUpperCase()}
+    </div>
+  );
+};
+
+Letter.propTypes = {
+  letter: PropTypes.string,
+  checkResult: PropTypes.number,
+};
+
 export default function Idiom(props) {
   let lettersLengthes = props.letters.map((item) => item.length);
   let lettersLength = lettersLengthes.reduce((p, c) => p + c, 0);
@@ -13,10 +35,25 @@ export default function Idiom(props) {
         gridTemplateColumns: `${column1Width}fr ${column2Width}fr ${column3Width}fr ${column4Width}fr`,
       }}
     >
-      <div className="bg-gray-200">A</div>
-      <div className="bg-gray-200">B</div>
-      <div className="bg-gray-200">C</div>
-      <div className="bg-gray-200">D</div>
+      {props.letters.map((letters, i) => {
+        return (
+          <div
+            className={`grid`}
+            style={{
+              gap: '2px',
+              gridTemplateColumns: `repeat(${letters.length}, 1fr)`,
+            }}
+          >
+            {letters.split('').map((letter, j) => {
+              let checkResult = null;
+              if (props.checkResult) {
+                checkResult = parseInt(props.checkResult[i].charAt(j));
+              }
+              return <Letter checkResult={checkResult} letter={letter} />;
+            })}
+          </div>
+        );
+      })}
     </div>
   );
 }
--- src/components/Board/Idiom/Idiom.stories.js
+++ src/components/Board/Idiom/Idiom.stories.js
@@ -9,7 +9,16 @@ const Template = ({ ...args }) => {
   return <Idiom {...args} />;
 };
 
-export const Default = Template.bind({});
-Default.args = {
+export const Current = Template.bind({});
+Current.argTypes = {
+  checkResult: { control: { disable: true } },
+};
+Current.args = {
   letters: ['yi', 'xin', 'er', 'yong'],
+};
+
+export const History = Template.bind({});
+History.args = {
+  letters: ['yi', 'xin', 'er', 'yong'],
+  checkResult: ['21', '001', '22', '0120'],
 };
