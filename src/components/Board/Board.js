0646f4934077b26e1c46c9bc57ce9f91f2a0436e
limboy <healdream@gmail.com>
2022-03-05 09:22:39 +0800

    update tone display

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -3,17 +3,90 @@ import { HiOutlineLightBulb } from 'react-icons/hi';
 import Idiom from './Idiom';
 import { useState } from 'react';
 import Modal from '../Modal';
+import { BsDash, BsSlash, BsChevronDown } from 'react-icons/bs';
+
+function Tone(props) {
+  let component;
+  let tone = parseInt(props.tone);
+  switch (tone) {
+    case 1:
+      component = <BsDash />;
+      break;
+    case 2:
+      component = <BsSlash />;
+      break;
+    case 3:
+      component = <BsChevronDown />;
+      break;
+    case 4:
+      component = (
+        <div style={{ transform: 'rotateY(180deg' }}>
+          <BsSlash />
+        </div>
+      );
+      break;
+    default:
+      break;
+  }
+  return component;
+}
+
+function Tip(props) {
+  return (
+    <Modal isOpen={props.isOpen} title="小提示" onClose={props.onClose}>
+      <div className="flex flex-col pt-6 pb-3 gap-1 items-center">
+        <p>以下是该成语的声调</p>
+        <p>每完成一次猜测，会多一个声调提示哦</p>
+        <div className="flex flex-row gap-4 pt-4">
+          {Array(4)
+            .fill(null)
+            .map((_, i) => {
+              let visibility =
+                props.attempts.history.length > i ? 'visible' : 'hidden';
+              return (
+                <div
+                  key={i}
+                  className="flex flex-col items-center border border-solid border-gray-700 w-[64px] h-[64px]"
+                  style={{
+                    fontFamily:
+                      '"Clear Sans", "Helvetica Neue", Arial, sans-serif"',
+                  }}
+                >
+                  <div
+                    className="text-3xl pt-1"
+                    style={{
+                      visibility,
+                    }}
+                  >
+                    {visibility === 'visible' ? (
+                      <Tone tone={props.config.answer.tone[i]} />
+                    ) : (
+                      '_'
+                    )}
+                  </div>
+                  <div
+                    className="text-sm text-gray-400"
+                    style={{
+                      visibility,
+                    }}
+                  >
+                    {visibility === 'visible'
+                      ? props.config.answer.tone[i] + ' 声'
+                      : '_'}
+                  </div>
+                </div>
+              );
+            })}
+        </div>
+      </div>
+    </Modal>
+  );
+}
 
 export default function Board(props) {
   let { attempts, config } = useAppContext();
   let [modalIsOpen, setModalIsOpen] = useState(false);
   let idioms = [];
-  const toneMap = {
-    1: 'ˉ',
-    2: 'ˊ',
-    3: 'ˇ',
-    4: 'ˋ',
-  };
 
   for (let i = 0; i < config.maxAttempts; i++) {
     if (i < attempts.history.length) {
@@ -42,62 +115,12 @@ export default function Board(props) {
             setModalIsOpen(true);
           }}
         ></HiOutlineLightBulb>
-        <Modal
+        <Tip
           isOpen={modalIsOpen}
-          title="小提示"
-          onClose={() => {
-            setModalIsOpen(false);
-          }}
-        >
-          <div className="flex flex-col pt-6 pb-3 gap-1 items-center">
-            <p>以下是该成语的声调</p>
-            <p>每完成一次猜测，会多一个声调提示哦</p>
-            <div className="flex flex-row gap-4 pt-4">
-              {Array(4)
-                .fill(null)
-                .map((_, i) => {
-                  let visibility =
-                    attempts.history.length > i ? 'visible' : 'hidden';
-                  return (
-                    <div
-                      key={i}
-                      className="relative border border-solid border-gray-700 w-[64px] h-[64px]"
-                      style={{
-                        fontFamily:
-                          '"Clear Sans", "Helvetica Neue", Arial, sans-serif"',
-                      }}
-                    >
-                      <div
-                        className=""
-                        style={{
-                          visibility,
-                          fontSize: '32px',
-                          position: 'absolute',
-                          left: '27px',
-                          top: '-2px',
-                        }}
-                      >
-                        {visibility === 'visible'
-                          ? toneMap[config.answer.tone[i]]
-                          : '_'}
-                      </div>
-                      <div
-                        className="text-4xl"
-                        style={{
-                          visibility,
-                          position: 'absolute',
-                          top: '18px',
-                          left: '22px',
-                        }}
-                      >
-                        {visibility === 'visible' ? config.answer.tone[i] : '_'}
-                      </div>
-                    </div>
-                  );
-                })}
-            </div>
-          </div>
-        </Modal>
+          onClose={() => setModalIsOpen(false)}
+          attempts={attempts}
+          config={config}
+        />
       </div>
       <div className="flex flex-col gap-2 md:gap-4">
         {idioms.map((idiom, i) => {
