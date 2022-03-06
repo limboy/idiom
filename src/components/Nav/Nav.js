80d86565772fc1f61ce86a494555372e6c3608e7
limboy <healdream@gmail.com>
2022-03-06 14:28:49 +0800

    add online playing count

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,8 +1,15 @@
 import { FiHelpCircle } from 'react-icons/fi';
 import { BsBarChartLine } from 'react-icons/bs';
 import PropTypes from 'prop-types';
+import useSWR from 'swr';
 
 export default function Nav(props) {
+  const onlineVisitorFetcher = (...args) =>
+    fetch(...args).then((res) => res.json());
+  const { data: visitorCount } = useSWR(
+    'https://plausible.io/api/stats/pinyincaichengyu.com/current-visitors',
+    onlineVisitorFetcher
+  );
   return (
     <div className="flex flex-row justify-between h-10 border-b border-gray-300 items-center py-6">
       <div>
@@ -12,8 +19,13 @@ export default function Nav(props) {
         />
       </div>
 
-      <div>
+      <div className="flex flex-col items-center justify-center">
         <h1 className="text-xl">{props.title ? props.title : '拼音猜成语'}</h1>
+        {visitorCount ? (
+          <p className="text-gray-400 text-xs">{visitorCount} playing</p>
+        ) : (
+          ''
+        )}
       </div>
 
       <div>
